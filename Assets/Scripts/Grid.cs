baf770d22ec1bf9621c5d6d6a64d1850823441f5
Stanisrawr <stlesk@GMAIL.COM>
2016-06-28 01:23:35 -0400

    Starting Build
    Following a unity tutorial to make a tetris game

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour {
+
+	public static int height=20;
+	public static int width=10;
+	public static Transform[,] grid = new Transform[width, height];
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public static Vector2 roundVector(Vector2 v)
+	{
+		return new Vector2 (Mathf.Round (v.x), Mathf.Round (v.y));
+
+
+	}
+	public static bool InsideBorder(Vector2 pos)
+	{
+		return((int)pos.x >= 0 && (int)pos.x < width && (int)pos.y >= 0);
+
+	}
+
+	public static void deleteRow(int y)
+	{
+
+		for (int x = 0; x < width; x++) 
+		{
+			Destroy(grid[x,y].gameObject);
+			grid [x, y] = null;
+
+		}
+
+	}
+
+	public static void decreaseRow(int y)
+	{
+
+	}
+}
+
+
---
+++ Assets/Scripts/SpawnNext.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnNext : MonoBehaviour {
+
+	public GameObject[] Group;
+
+	void Start() {
+		// Spawn initial Group
+		Spawn_Next();
+	}
+
+
+	public void Spawn_Next()
+	{
+		int rand;
+		rand = Random.Range (0, 6);
+
+		Instantiate (Group [rand],transform.position,Quaternion.identity);
+
+
+
+	}
+
+
+}
